bc79b69bd1f42895c4382a2ab29f7f25bfda5f7f
Bishnupkl <bishnup212@gmail.com>
2020-09-13 16:27:06 +0545

    register, login and logout function added succesfully

--- app/Http/Controllers/auth/Authcontroller.php
+++ app/Http/Controllers/auth/Authcontroller.php
@@ -8,5 +8,62 @@ use Illuminate\Http\Request;
 class Authcontroller extends Controller
 {
 
+     public function register(Request $request)
+   {
+          $request->validate([
+                 'fName' => 'required|string',
+                 'lName' => 'required|string',
+                 'email' => 'required|string|email|unique:users',
+                 'password' => 'required|string'
+          ]);
+          $user = new User;
+          $user->first_name = $request->fName;
+          $user->last_name = $request->lName;
+          $user->email = $request->email;
+          $user->password = bcrypt($request->password);
+          $user->save();
+          return response()->json([
+               'message' => 'Successfully created user!'
+          ], 201);
+   }
 
+   public function login(Request $request) {
+        $request->validate([
+             'email' => 'required|string|email',
+             'password' => 'required|string'
+           ]);
+        $credentials = request(['email', 'password']);
+     // print_r($credentials);die;
+     if(!Auth::attempt($credentials))
+         return response()->json([
+            'message' => 'Unauthorized'
+         ],401);
+     $user = $request->user();
+     $tokenResult = $user->createToken('Personal Access Token');
+     $token = $tokenResult->token;
+     if ($request->remember_me)
+         $token->expires_at = Carbon::now()->addWeeks(1);
+     $token->save();
+     return response()->json([
+         'access_token' => $tokenResult->accessToken,
+         'token_type' => 'Bearer',
+         'expires_at' => Carbon::parse(
+             $tokenResult->token->expires_at
+          )->toDateTimeString()
+      ]);
+   }
+
+    public function logout(Request $request)
+    {
+        $request->user()->token()->revoke();
+        return response()->json([
+            'message' => 'Successfully logged out']);
 }
+    public function user(Request $request)
+    {
+        return response()->json($request->user());
+    }
+
+
+
+};
